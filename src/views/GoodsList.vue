95907affee0af12e16ee2d86873ee9c43533cd2a
jiaen188 <244189981@qq.com>
2018-03-31 19:45:38 +0800

    footer组件

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -102,34 +102,14 @@
             </div>
         </div>
 
-        <footer class="footer">
-            <div class="footer__wrap">
-                <div class="footer__secondary">
-                    <div class="footer__inner">
-                        <div class="footer__region">
-                            <span>Region</span>
-                            <select class="footer__region__select">
-                                <option value="en-US">USA</option>
-                                <option value="zh-CN">China</option>
-                                <option value="in">India</option>
-                            </select>
-                        </div>
-                        <div class="footer__secondary__nav">
-                            <span>Copyright © 2017 IMooc All Rights Reserved.</span>
-                            <a href="http://us.lemall.com/us/aboutUs.html">About Us</a>
-                            <a href="http://us.lemall.com/us/termsofUse.html">Terms &amp; Conditions</a>
-                            <a href="http://us.lemall.com/us/privacyPolicy.html">Privacy Policy</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <nav-footer></nav-footer>
 
     </div>
 </template>
 
 <script>
-import NavHeader from './../components/Header';
+import NavHeader from './../components/NavHeader';
+import NavFooter from './../components/NavFooter';
 
 export default {
     name: 'goodList',
@@ -139,7 +119,8 @@ export default {
         }
     },
     components: {
-        NavHeader
+        NavHeader,
+        NavFooter
     }
 }
 </script>
